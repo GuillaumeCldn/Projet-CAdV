fab0592cccd6e68d9cc93f292699119bcd125bc0
ulyssebourdicgirard <ulysse.bourdic.girard@gmail.com>
2026-01-06 17:03:58 +0100

    Modification 17:00 parallélisation

--- Code/mainProjetCADV_2a.m
+++ Code/mainProjetCADV_2a.m
@@ -34,10 +34,10 @@ VaTrim = utEas2Tas(Eas_KTS*KTS2MS, hTrimFL*FL2M);
 hTrim = hTrimFL*FL2M;
 dthrIdle = 0.1;
 trimVal = utComputeTrimIdle(hTrim,VaTrim,aircraftChosen,km,ms,dthrIdle); %trim thrust idle
-dPHRTrim = trimVal(1) %rad
-alphaTrim = trimVal(2) %rad
-thetaTrim = trimVal(3) %rad
-fpaDeg = RAD2DEG*(thetaTrim-alphaTrim) %pente, flight path angle
+dPHRTrim = trimVal(1); %rad
+alphaTrim = trimVal(2); %rad
+thetaTrim = trimVal(3); %rad
+fpaDeg = RAD2DEG*(thetaTrim-alphaTrim); %pente, flight path angle
 
 %Linearisation
 %vecteur d'etat au point de trim
@@ -50,26 +50,26 @@ xTrim(itheta) = thetaTrim; %rad
 uTrim = zeros(TOTAL_CMD, 1);
 uTrim(idPHR) = dPHRTrim;
 uTrim(ithr) = dthrIdle;
-uTrim(idelevator) = 0
+uTrim(idelevator) = 0;
 
 % Vérification trim
-xdotTrim = utAcDynamicsFunction(xTrim,uTrim,aircraftChosen,km,ms)
+xdotTrim = utAcDynamicsFunction(xTrim,uTrim,aircraftChosen,km,ms);
 
 % Linéarisation autour du point de trim
-[A, B, C, D] = linmod('acDynModel_ToLinearize_2015',xTrim, uTrim)
+[A, B, C, D] = linmod('acDynModel_ToLinearize_2015',xTrim, uTrim);
 
 
-A4 = A(iVa:iq, iVa:iq)
-B4 = B(iVa:iq, idelevator)
+A4 = A(iVa:iq, iVa:iq);
+B4 = B(iVa:iq, idelevator);
 
-C4 = [0, 0, 1, 0]
-D4 = 0
+C4 = [0, 0, 1, 0];
+D4 = 0;
 
-[state4num, state4den] = ss2tf(A4, B4, C4, D4)
-state4 = tf(state4num, state4den)
+[state4num, state4den] = ss2tf(A4, B4, C4, D4);
+state4 = tf(state4num, state4den);
 
 % Calcul des modes de A4
-modes4 = eig(A4)
+modes4 = eig(A4);
 [Wn, zeta] = damp(A4);
  % Nom des modes ? Deux pôles normaux et deux pôles très lents, principalement oscillatoires ? 
 
@@ -81,44 +81,50 @@ D = 0.05; % %
 % Préfiltre
 FTBF_in = feedback(state4, tf([Kd, 0], 1)); % Fonction de transfert 'interne'
 FTBF = feedback(tf([Kd, Ki], [1, 0])*FTBF_in, 1);
-pole(FTBF)
-zero(FTBF)
+pole(FTBF);
+zero(FTBF);
 
-minFTBF = minreal(FTBF, 1e-4)
+minFTBF = minreal(FTBF, 1e-4);
 
 q = minFTBF.den{1}; % Dénominateur de la FTBF
-Cpf = tf(q(end), minFTBF.num{1})
-Cpf1 = tf(1,[dp,1])
+Cpf = tf(q(end), minFTBF.num{1});
+Cpf1 = tf(1,[dp,1]);
 
 
 %% Commande par retour d'état
 
-%Linearisation
-%vecteur d'etat au point de trim
+% Linearisation
+
+% Vecteur d'etat au point de trim
 xTrimcm = zeros(8, 1);
 xTrimcm(ihp) = hTrim; %m
 xTrimcm(iVa) = VaTrim; %m/sec
 xTrimcm(ialpha) = alphaTrim; %rad
 xTrimcm(itheta) = thetaTrim; %rad
-%vecteur de commande au point de trim choisi
-uTrimcm = 0
 
-%linearisation autour du point de trim
-[Acm, Bcm, Ccm, Dcm] = linmod('acDynModel_ToLinearize_2015',xTrimcm, uTrimcm)
+% Vecteur de commande au point de trim choisi
+uTrimcm = zeros(TOTAL_CMD, 1);
+uTrimcm(idPHR) = dPHRTrim;
+uTrimcm(ithr) = dthrIdle;
+uTrimcm(idelevator) = 0; % Ou votre valeur de trim d'élévateur
 
-%Commandablite du systeme
-rank(ctrb(Acm,Bcm))
+% Linearisation autour du point de trim
+[Acm, Bcm, Ccm, Dcm] = linmod('acDynModelGolfTenueClassique',xTrimcm, uTrimcm);
 
-Kcm = place(Acm,Bcm,[dp,conj(dp),-5,-6,-7,-8,-9,-10])
-eig(Acm-Bcm*Kcm)
-%Observabilité du systeme
-rank(obsv(Acm,Ccm))
+% Commandablite du systeme
+rank(ctrb(Acm,Bcm));
 
-A6 = Acm(iVa:end,iVa:end)
-B6 = Bcm(iVa:end )
+% Observabilité du systeme
+rank(obsv(Acm,Ccm));
 
-%precommande
-H = -inv(Ccm*inv(Acm-Bcm*Kcm)*Bcm)
+A6 = Acm(iVa:end,iVa:end);
+B6 = Bcm(iVa:end,idelevator);
+C6 = Ccm(iVa:end,iVa:end);
+pA6 = eig(A6)
+Kcm = place(A6,B6,[dp, conj(dp), -3.3943 + 0.1544i, -3.3943 - 0.1544i, -5, -6, -7, -8]);
+
+% Precommande
+H = -inv(Ccm*inv(A6-B6*K6)*B6);
 
 
 
